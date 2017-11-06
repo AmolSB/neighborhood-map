f8cedc4795b723384c0cb69c3c480289a92eefca
AmolSB <amolborgaonkar333@gmail.com>
2017-11-06 20:42:31 +0530

    Shows Points of interests

--- js/app.js
+++ js/app.js
@@ -5,10 +5,7 @@ var MapApplication = function() {
 	var map;
 	var	localLocation = {lat: -37.810432, lng: 144.96616};
 
-	self.locations = ko.observableArray([
-		{name: 'Museum', coord: {lat: -37.669012, lng: 144.841027}},
-		{name: 'Ariport', coord: {lat: -37.669012, lng: 144.841027}}
-	]);
+	self.locations = ko.observableArray();
 
 	var configureBindingHandlers = function() {
 		ko.bindingHandlers.mapPanel = {
@@ -27,6 +24,19 @@ var MapApplication = function() {
 			map: map
 		});
 	}
+
+	var url = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=51.513144,-0.124396&radius=2520&time=any&v=20150409&m=swarm&limit=5&sortByDistance=1&offset=0';
+
+	fetch(url).then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		console.log(data.response.groups[0].items)
+		for (var i=0; i<data.response.groups[0].items.length; i++) {
+			self.locations.push(data.response.groups[0].items[i].venue);
+		}
+	});
+
+	console.log(self.locations());
 
 	// return {
 		// myname: self.myname
