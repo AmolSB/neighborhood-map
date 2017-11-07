8575fcc3ccce7c7aef11bb873d38e27dcb52a449
AmolSB <amolborgaonkar333@gmail.com>
2017-11-07 09:43:24 +0530

    Show Top Picks, Restaurants and Shops (show marker works)

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ var MapApplication = function() {
 	self = this;
 	var map;
 	var	localLocation = {lat: 51.513144, lng: -0.124396};
-	self.locations = ko.observableArray();
+	self.topPicks = ko.observableArray();
+	self.restaurants = ko.observableArray();
+	self.shops = ko.observableArray();
 
 	var configureBindingHandlers = function() {
 		ko.bindingHandlers.mapPanel = {
@@ -26,18 +28,33 @@ var MapApplication = function() {
 		});
 	}
 
-	var url = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=51.513144,-0.124396&radius=2520&time=any&v=20150409&m=swarm&limit=5&sortByDistance=1&offset=0';
+	var topPicksURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=51.513144,-0.124396&radius=2520&time=any&v=20150409&m=swarm&section=topPicks&limit=5&sortByDistance=1&offset=0';
+	var restaurantsURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=51.513144,-0.124396&radius=2520&time=any&v=20150409&m=swarm&section=food&limit=5&sortByDistance=1&offset=0';
+	var shopsURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=51.513144,-0.124396&radius=2520&time=any&v=20150409&m=swarm&section=shops&limit=5&sortByDistance=1&offset=0';
 
-	fetch(url).then(function(response) {
+	fetch(topPicksURL).then(function(response) {
 		return response.json();
 	}).then(function(data) {
-		console.log(data.response.groups[0].items)
-		for (var i=0; i<data.response.groups[0].items.length; i++) {
-			self.locations.push(data.response.groups[0].items[i].venue);
-		}
+		addItems(data, self.topPicks);
 	});
 
-	console.log(self.locations());
+	fetch(restaurantsURL).then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		addItems(data, self.restaurants);
+	});
+
+	fetch(shopsURL).then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		addItems(data, self.shops);
+	});
+
+	var addItems = function(data, array) {
+		for (var i=0; i<data.response.groups[0].items.length; i++) {
+			array.push(data.response.groups[0].items[i].venue);
+		}
+	}
 
 	// return {
 		// myname: self.myname
