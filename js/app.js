58b446790b30fdf0b682da33e208a0d7b6d2074d
AmolSB <amolborgaonkar333@gmail.com>
2017-11-10 21:04:31 +0530

    Complete. Finishing Remains

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var MapApplication = function() {
 
 	self = this;
 	var map;
-	var	localLocation = {lat: 51.513144, lng: -0.124396};
+	var	localLocation = {lat: 48.8566, lng: 2.3522};
 	self.topPicks = ko.observableArray();
 	self.restaurants = ko.observableArray();
 	self.shops = ko.observableArray();
@@ -11,16 +11,19 @@ var MapApplication = function() {
 	self.topPicksMarkers = ko.observableArray();
 	self.restaurantsMarkers = ko.observableArray();
 	self.shopsMarkers = ko.observableArray();
-	var marker;
+	var marker = new google.maps.Marker();
 	var flag = false;
 	self.allLocations = ko.observableArray();
+	var infowindow = new google.maps.InfoWindow({
+			pixelOffset: new google.maps.Size(0, -20)
+	});
 
 	var configureBindingHandlers = function() {
 		ko.bindingHandlers.mapPanel = {
 			init: function(element, valueAccessor) {
 				map = new google.maps.Map(element, {
 					center: localLocation,
-					zoom: 12,
+					zoom: 13,
 					mapTypeControl: true,
 					mapTypeControlOptions: {
       				  style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
@@ -37,26 +40,40 @@ var MapApplication = function() {
 		}
 	}
 
+	var showInfoWindow = function(obj) {
+		var infoWindowLocation = new google.maps.LatLng(obj.location.lat, obj.location.lng);
+		console.log(infoWindowLocation);
+		infowindow.setPosition(infoWindowLocation);
+		infowindow.setOptions({content: `<div class="infowindow">
+			<h6>${obj.name}</h6>
+			<p>${obj.categories[0].name}</p>
+			<p>${obj.hours.status}</p>
+			<p>${obj.location.address}</p>
+			<a href="${obj.url}">Go to Website</a>
+			</div>`})
+		infowindow.open(map);
+	}
+
+	var showMarkerInfoWindow = function(marker) {
+		for(var i=0; i<self.topPicks().length; i++) {
+			if(marker.loc == self.topPicks()[i].name) {
+				showInfoWindow(self.topPicks()[i])
+			}
+		}
+	}
+
 	self.filteredPlaces = ko.computed(function() {
 		var nameSearch = self.nameSearch();
 		var result = []
 
 		if(!nameSearch) {
 			showAllMarkers();
-			// for(var i=0; i<self.topPicksMarkers().length; i++) {
-			// 	result.push(self.topPicksMarkers()[i].loc);
-			// }
 			return self.topPicks();
-			// return result;
 		}
 
-		// for(var i=0; i<self.topPicks().length; i++) {
-		// 	if(self.topPicks()[i].name.toLowerCase().includes(nameSearch.toLowerCase())){
-		// 		result.push(self.topPicks()[i]);
-		// 	}
-		// }
 
 		for(var i=0; i<self.topPicks().length; i++) {
+			console.log(typeof(self.topPicksMarkers()[i]))
 			// var place = self.topPicksMarkers()[i];
 			if(self.topPicks()[i].name.toLowerCase().includes(nameSearch.toLowerCase())) {
 				result.push(self.topPicks()[i])
@@ -68,118 +85,78 @@ var MapApplication = function() {
 			// console.log(markers[i].getPosition().lat());
 		}
 
-		// self.topPicksMarkers().forEach(function(v, i) {
-		// 	if(v.loc.toLowerCase().includes(nameSearch.toLowerCase())) {
-		// 		result.push(self.topPicks()[i])
-		// 		v.setVisible(true);
-		// 	} else {
-		// 		v.setVisible(false);
-		// 	}
-		// })
 		return result;
 	});
 
 	// console.log(self.filteredPlaces());
 
-// 	self.locationsFilter = ko.computed(function() {
-//         var result = [];
-//         for (var i = 0; i < this.markers.length; i++) {
-//             var markerLocation = this.markers[i];
-//             if (markerLocation.title.toLowerCase().includes(this.searchOption()
-//                     .toLowerCase())) {
-//                 result.push(markerLocation);
-//                 this.markers[i].setVisible(true);
-//             } else {
-//                 this.markers[i].setVisible(false);
-//             }
-//         }
-//         return result;
-// }, this)
-
-	self.showMarker = function(obj) {
- 		var placeLatLng = new google.maps.LatLng(obj.location.lat, obj.location.lng);
-		var marker = new google.maps.Marker({
-			position: placeLatLng,
-			map: map
-		});
-
-	}
-
-	self.doSomething = function(obj) {
-		flag = true;
-		self.showMarker(obj);
-		self.hideOtherMarkers(obj);
-	}
-
-	// self.hideMarker = function(obj) {
-	// 	var placeLatLng = new google.maps.LatLng(obj.location.lat, obj.location.lng);
-	// 	var marker = new google.maps.Marker({
+	// self.showMarker = function(obj) {
+ // 		var placeLatLng = new google.maps.LatLng(obj.location.lat, obj.location.lng);
+	// 	marker = new google.maps.Marker({
 	// 		position: placeLatLng,
-	// 		map: null
+	// 		map: map
 	// 	});
 	// }
 
-	self.hideOtherMarkers = function(obj) {
-		for(var i=0; i<self.restaurantsMarkers().length; i++) {
-			if(obj.name !== self.restaurantsMarkers()[i].loc) {
-				self.restaurantsMarkers()[i].setVisible(false);
-			}
-		}
+	self.showMarkerAndInfoWindow = function(obj) {
+		// flag = true;
+		// self.showMarker(obj);
+		// self.hideOtherMarkers(obj);
+		getMarker(obj);
+		showInfoWindow(obj);
+	}
 
-		for(var j=0; j<self.topPicksMarkers().length; j++) {
-			self.topPicksMarkers()[j].setVisible(false);
+	var getMarker = function(obj) {
+		for(var i=0; i<self.topPicksMarkers().length; i++) {
+			if(obj.name == self.topPicksMarkers()[i].loc) {
+				self.topPicksMarkers()[i].setAnimation(google.maps.Animation.DROP);
+			}
 		}
 	}
 
-	// self.combineLocations = function() {
-	// 	for(var i=0; i<self.topPicksMarkers().length; i++) {
-	// 		self.allLocations.push(self.topPicksMarkers()[i]);
-	// 	}
-	// }
+	self.hideOtherMarkers = function(obj) {
+		for(var i=0; i<self.topPicksMarkers().length; i++) {
+			if(obj.name !== self.topPicksMarkers()[i].loc) {
+				self.topPicksMarkers()[i].setVisible(false);
+			}
+		}
+	}
 
-	// self.combineLocations();
+	var getPlaceByMarker = function(obj) {
+		for(var i=0; i<self.topPicks().length; i++) {
+			if(obj.loc == self.topPicks()[i].name){
+				return self.topPicks()[i];
+			}
+		}
+	}
 
-	// console.log(self.allLocations());
-
-
-	var topPicksURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=51.513144,-0.124396&radius=25000&time=any&v=20150409&m=swarm&section=topPicks&limit=4';
-	var restaurantsURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=51.513144,-0.124396&radius=25000&time=any&v=20150409&m=swarm&section=food&limit=3';
-	var shopsURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=51.513144,-0.124396&radius=25000&time=any&v=20150409&m=swarm&section=shops&limit=3';
+	var topPicksURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=48.8566,2.3522&radius=10000&time=any&v=20150409&m=swarm&section=topPicks&limit=10';
+	var restaurantsURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=48.8566,2.3522&radius=10000&time=any&v=20150409&m=swarm&section=food&limit=3';
+	var shopsURL = 'https://api.foursquare.com/v2/venues/explore?client_id=K2JWQRQEIQT2M5HTJGBW3TQXNSOU1EI3SAPO0DDMNLMT24DD&client_secret=FXX3ZZXCVCJUYILHGIB2CSTKVDK51XXLOL4WOZUFFKN52AYE&ll=48.8566,2.3522&radius=10000&time=any&v=20150409&m=swarm&section=shops&limit=3';
 
 	fetch(topPicksURL).then(function(response) {
 		return response.json();
 	}).then(function(data) {
 		addItems(data, self.topPicks);
 		addTopPicksMarkers(data, self.topPicksMarkers);
-	}).then(function() { 
-		fetch(restaurantsURL).then(function(response) {
-			return response.json();
-		}).then(function(data) {
-			addItems(data, self.topPicks);
-			addTopPicksMarkers(data, self.topPicksMarkers);
-		});
-	}).then(function() {
-		fetch(shopsURL).then(function(response) {
-			return response.json();
-		}).then(function(data) {
-			addItems(data, self.topPicks);
-			addTopPicksMarkers(data, self.topPicksMarkers);
-		});
-	});
-
-	// fetch(restaurantsURL).then(function(response) {
-	// 	return response.json();
-	// }).then(function(data) {
-	// 	addItems(data, self.restaurants);
-	// 	addRestaurantsMarkers(data, self.restaurantsMarkers);
-	// });
-
-	// fetch(shopsURL).then(function(response) {
-	// 	return response.json();
-	// }).then(function(data) {
-	// 	addItems(data, self.shops);
-	// });
-
+	}).catch(function(error) {
+		alert("Problem loading FourSquare API");
+	});//.then(function() { 
+	// 	fetch(restaurantsURL).then(function(response) {
+	// 		return response.json();
+	// 	}).then(function(data) {
+	// 		addItems(data, self.topPicks);
+	// 		addTopPicksMarkers(data, self.topPicksMarkers);
+	// 	});
+	// }).then(function() {
+	// 	fetch(shopsURL).then(function(response) {
+	// 		return response.json();
+	// 	}).then(function(data) {
+	// 		addItems(data, self.topPicks);
+	// 		addTopPicksMarkers(data, self.topPicksMarkers);
+	// 	});
+	// })
+	
 	var addItems = function(data, array) {
 		for (var i=0; i<data.response.groups[0].items.length; i++) {
 			array.push(data.response.groups[0].items[i].venue);
@@ -189,38 +166,25 @@ var MapApplication = function() {
 	var addTopPicksMarkers = function(data, array) {
 		for(var i=0; i<data.response.groups[0].items.length; i++) {
 				marker = new google.maps.Marker({
-				position: {lat: data.response.groups[0].items[i].venue.location.lat, lng: data.response.groups[0].items[i].venue.location.lng},
-				map: map,
-				loc: self.topPicks()[i].name,
-				title: 'top pick'
-			});
-			array.push(marker);
-		}
-	}
-
-	var addRestaurantsMarkers = function(data, array) {
-		for(var i=0; i<data.response.groups[0].items.length; i++) {
-				marker = new google.maps.Marker({
-				position: {lat: data.response.groups[0].items[i].venue.location.lat, lng: data.response.groups[0].items[i].venue.location.lng},
-				loc: self.restaurants()[i].name,
-				title: 'restaurant'
-			});
+					position: {lat: data.response.groups[0].items[i].venue.location.lat, lng: data.response.groups[0].items[i].venue.location.lng},
+					map: map,
+					animation: google.maps.Animation.DROP,
+					loc: data.response.groups[0].items[i].venue.name
+				});
+				marker.addListener('click', function() {
+					this.setAnimation(google.maps.Animation.DROP);
+					showInfoWindow(getPlaceByMarker(this));
+				})
 			array.push(marker);
 		}
 	}
 
 	console.log(self.topPicksMarkers());
-	console.log(self.topPicks())
-	// return {
-		// myname: self.myname
-	// }
+	console.log(self.topPicks());
 }
 
 ko.applyBindings(new MapApplication());
 
-$("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("active");
-});
+
 
 $(".button-collapse").sideNav();
